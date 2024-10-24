428cbd4f0d12f70bcbef0688af826b066d35ef1d
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2024-10-24 17:07:52 +0200

    Update GooglePayButton.vue

--- apps/web/components/PayPal/GooglePayButton.vue
+++ apps/web/components/PayPal/GooglePayButton.vue
@@ -163,7 +163,7 @@ async function processPayment(paymentData: google.payments.api.PaymentData) {
       });
       if (!order || !order.order || !order.order.id) throw new Error('Order creation failed.');
       await executeOrder({
-        mode: 'google_pay',
+        mode: 'paypal',
         plentyOrderId: Number.parseInt(orderGetters.getId(order)),
         paypalTransactionId: transaction.id,
       });
